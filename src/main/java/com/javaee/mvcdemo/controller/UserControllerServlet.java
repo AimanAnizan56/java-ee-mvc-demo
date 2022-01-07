f0127303a58bc63139b0f13c42ec27363ddf12bf
Aiman Anizan <anizanaimann1@gmail.com>
2022-01-08 01:57:25 +0800

    Add login function (trash code)

--- src/main/java/com/javaee/mvcdemo/controller/UserControllerServlet.java
+++ src/main/java/com/javaee/mvcdemo/controller/UserControllerServlet.java
@@ -1,12 +1,10 @@
 package com.javaee.mvcdemo.controller;
 
-import org.json.JSONObject;
-
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import java.io.Console;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet(name = "UserControllerServlet", value = "/UserControllerServlet")
 public class UserControllerServlet extends HttpServlet {
@@ -18,5 +16,25 @@ public class UserControllerServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("doPost");
+
+        //get action
+        String action = request.getParameter("action");
+
+        switch (action.toLowerCase()) {
+            case "login":
+                String username = request.getParameter("username");
+                String password = request.getParameter("password");
+
+                login(username, password);
+            break;
+            default:
+
+            break;
+        }
+    }
+
+    private void login(String username, String password) {
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
     }
 }
